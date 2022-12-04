fb48c2981f8fa687300681747600afd246ae669a
chavezAlexis <78992181+chavezAlexis@users.noreply.github.com>
2022-12-03 19:50:45 -0600

    Add files via upload

--- models/Server.js
+++ models/Server.js
@@ -1,1 +1,39 @@
+const express = require('express');
+const dbConnection = require('../database/dbConnection');
+const cors = require('cors');
 
+class Server {
+    constructor()
+    {
+        this.app = express();
+        this.port = process.env['PORT'] || 3000;
+        this.mainPath = '/api';  
+        this.middlewares();
+        this.mongoConnect();
+        this.routes();
+    }
+    middlewares()
+    {
+        this.app.use(express.json());
+        this.app.use(cors());
+    }
+    routes()
+    {
+        this.app.use(`${this.mainPath}/comments`, require('../routes/comments.routes'));
+        this.app.use(`${this.mainPath}/items`, require('../routes/items.routes.js'));
+        this.app.use(`${this.mainPath}/users`, require('../routes/users.routes.js'));
+    }
+    async mongoConnect()
+    {
+        await dbConnection();
+    }
+    listen()
+    {
+        this.app.listen(this.port, () => {
+            console.log(`application listen on port ${this.port}`)
+        })
+    }
+}
+
+
+module.exports = Server;
